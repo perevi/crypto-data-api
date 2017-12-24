29325242bac748e8957c07bddbf427d034220bc1
perevi <v.j.f.perez@gmail.com>
2017-12-23 21:15:51 -0500

    Add saving option

--- downloader.py
+++ downloader.py
@@ -7,7 +7,14 @@ class downloader:
     def __init__(self, source):
         self.source = source
 
-    def get_daily_data(self, symbol, market):
+    def gen_file_name(self, head):
+        """
+        :param signature: str
+        :return: str
+        """
+        return f'./data/{head}.hdf'
+
+    def get_daily_data(self, symbol, market, save=False, filename=None):
         """
         :param symbol: str  eg. BTC
         :param market: str  eg. USD
@@ -17,12 +24,21 @@ class downloader:
             df = alphavantage.get_daily_data(symbol, market)
         else:
             raise NameError(f'API not defined for:{self.source}')
+        df['source'] = self.source
+
+        if(save):
+            if(filename==None):
+                filename = self.gen_file_name(f'{symbol}-{market}.daily.{source}')
+            df.to_hdf(filename, 'index')
         return df
 
 
 if __name__ == '__main__':
-    mydownloader = downloader('alphavantage')
-    df = mydownloader.get_daily_data('BTC', 'USD')
+    source = 'alphavantage'
+    symbol = 'BTC'
+    market = 'USD'
+    mydownloader = downloader(source)
+    df = mydownloader.get_daily_data(symbol, market, save=True)
     print(df.head())
 
 
